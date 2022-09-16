a95bfb976b845e7e37ce579f5d259393be000ddc
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-16 12:00:31 -0700

    add and import header

---
+++ leader-bet/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <React.Fragment>
+      <nav class="navbar bg-light">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">LeaderBet</span>
+          <span>Login</span>
+        </div>
+      </nav>
+    </React.Fragment>
+  );
+}
+
+export default Header;
