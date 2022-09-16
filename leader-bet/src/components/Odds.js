59f19f49c6abae3059953c57f49b15a010b66bdc
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-16 15:46:46 -0700

    add Odds component

--- leader-bet/src/components/Odds.js
+++ leader-bet/src/components/Odds.js
@@ -4,22 +4,25 @@ function Odds() {
   return (
     <React.Fragment>
       <div className="card">
-        <div className="buttoneContaienr">
-          <div>
-            <button className="sports">NFL</button>
-          </div>
-          <div>
-            <button className="sports">MLB</button>
-          </div>
-          <div>
-            <button className="sports">NHL</button>
-          </div>
+        <div className="card-header text-center">
+          <h1>Odds</h1>
+        </div>
+        <div className="d-flex justify-content-around mt-5 mb-4">
+            <div>
+              <button className="sports btn btn-outline-primary">NFL</button>
+            </div>
+            <div>
+              <button className="sports btn btn-outline-primary">MLB</button>
+            </div>
+            <div>
+              <button className="sports btn btn-outline-primary">NHL</button>
+            </div>
         </div>
         <div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
+            <li class="list-group-item">first game</li>
+            <li class="list-group-item">second game</li>
+            <li class="list-group-item">third game</li>
           </ul>
         </div>
       </div>
