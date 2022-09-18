3d9c844d867c60595ef47f7e1c8efd359336d1ba
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-17 20:44:05 -0700

    added forgot password and register links

--- leader-bet/src/components/Login.js
+++ leader-bet/src/components/Login.js
@@ -2,32 +2,35 @@ import React from "react";
 
 function Login() {
 
-  const loginStyle = {
-    width:"250px"
-  }
-
-  const marginTop = {
-    marginTop:"250px"
+  const cardWidth = {
+    width: "18rem",
+    marginTop: "250px"
   }
 
   return (
     <React.Fragment>
-      <div className="d-flex justify-content-center" style={marginTop}>
-        <div style={loginStyle} className="text-center">
-          <h3 className="mb-3">Login</h3>
-          <form className="form">
-              <div className="mb-3">
-                <input type="email" className="form-control col-xs-3" id="exampleInputEmail1" size="50" aria-describedby="emailHelp" placeholder="email"></input>
-              </div>
-              <div className="mb-3">
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="password"></input>
-              </div>
-            <button type="submit" className="btn btn-outline-primary">Login</button>
-            </form>
+      <div>
+        <div className="card text-center mx-auto p-3" style={cardWidth}>
+          <div className="">
+            <h3 className="mb-3 card-title">Login</h3>
+            <form className="form">
+                <div className="mb-3">
+                  <input type="email" className="form-control col-xs-3" id="exampleInputEmail1" size="50" aria-describedby="emailHelp" placeholder="email"></input>
+                </div>
+                <div className="mb-3">
+                  <input type="password" className="form-control" id="exampleInputPassword1" placeholder="password"></input>
+                  <p className="small">forgot password?</p>
+                </div>
+              <button type="submit" className="btn btn-outline-primary">Login</button>
+              </form>
+          </div>
         </div>
+        <p className="text-center mt-3">New to LeaderBet? Register an account here!</p>
       </div>
     </React.Fragment>
   );
 };
+
+//zz
 
 export default Login;
