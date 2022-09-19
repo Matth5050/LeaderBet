7a667e0deeabf21d6bb9191a33e94d3f19f74c1e
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-18 23:01:05 -0700

    added some routes

--- leader-bet/src/components/App.js
+++ leader-bet/src/components/App.js
@@ -3,24 +3,31 @@ import Header from './Header';
 import LeaderBoard from './LeaderBoard';
 import Odds from './Odds';
 import Footer from './Footer';
-
+import Login from './Login';
+import Register from './Register';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
-      <Header/>
-      <div className='container'>
-        <div className='row mt-5'>
-          <div className='col-9'>
-            <Odds />
-          </div>
-          <div className='col-3'>
-            <LeaderBoard />
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </React.Fragment>
+    <Router>
+     
+        <Routes>
+            {/* <div className='container'>
+                  <div className='row mt-5'>
+                    <div className='col-9'>
+                      <Odds />
+                    </div>
+                    <div className='col-3'>
+                      <LeaderBoard />
+                    </div>
+                  </div>
+                </div> */}
+          <Route path="/" element={<Odds />} />    
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+    </Router>
   );
 }
 
