82275b02ceaf15eae5455feaa9077fe3eca6b961
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-18 23:01:30 -0700

    updated header to be dynamic based on auth status

--- leader-bet/src/components/Header.js
+++ leader-bet/src/components/Header.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import { Link } from "react-router-dom";
+import { auth } from "./../firebase.js";
 
 function Header() {
-  return (
-    <React.Fragment>
+
+  if (auth.currentUser === null) {
+    return (
+      <React.Fragment>
       <nav class="navbar bg-light">
         <div class="container-fluid">
-          <span class="navbar-brand mb-0 h1">LeaderBet</span>
-          <span>Login</span>
+          <span class="navbar-brand mb-0 h1"><Link to="/">LeaderBet</Link></span>
+          <span class="mb-0 h1"><Link to="/login">Login</Link></span>
+          <span class="mb-0 h1"><Link to="/register">Register</Link></span>
         </div>
       </nav>
     </React.Fragment>
-  );
+    )
+  } else if (auth.currentUser !== null) {
+    return (
+      <React.Fragment>
+      <nav class="navbar bg-light">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1"><Link to="/">LeaderBet</Link></span>
+          <span class="mb-0 h1">`${auth.userEmail}`</span>
+        </div>
+      </nav>
+    </React.Fragment>
+    )
+  }
 }
 
 export default Header;
