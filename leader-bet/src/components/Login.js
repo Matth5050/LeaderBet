5a36e99b2dc38b8ea5e45f381f05773602788f7a
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-18 22:19:58 -0700

    added login component

--- leader-bet/src/components/Login.js
+++ leader-bet/src/components/Login.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "./../firebase.js";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 function Login() {
 
@@ -7,21 +9,50 @@ function Login() {
     marginTop: "250px"
   }
 
+  
+  const [signInSuccess, setSignInSuccess] = useState(null);
+  
+  function doSignIn(event) {
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setSignInSuccess(`You've successfully signed in as ${userCredential.user.email}`)
+      })
+      .catch((error) => {
+        setSignInSuccess(`There was an error signing in: ${error.message}!`)
+      });
+  }
+
   return (
     <React.Fragment>
       <div>
+      {signInSuccess}
         <div className="card text-center mx-auto p-3" style={cardWidth}>
           <div className="">
             <h3 className="mb-3 card-title">Login</h3>
-            <form className="form">
+            <form className="form" onSubmit={doSignIn}>
                 <div className="mb-3">
-                  <input type="email" className="form-control col-xs-3" id="exampleInputEmail1" size="50" aria-describedby="emailHelp" placeholder="email"></input>
+                  <input 
+                  type="email" 
+                  className="form-control col-xs-3"
+                  name="signinEmail" 
+                  id="exampleInputEmail1" 
+                  size="50" 
+                  aria-describedby="emailHelp" 
+                  placeholder="email"></input>
                 </div>
                 <div className="mb-3">
-                  <input type="password" className="form-control" id="exampleInputPassword1" placeholder="password"></input>
+                  <input 
+                  type="password" 
+                  name="signinPassword"
+                  className="form-control" 
+                  id="exampleInputPassword1" 
+                  placeholder="password"></input>
                   <p className="small">forgot password?</p>
                 </div>
-              <button type="submit" className="btn btn-outline-primary">Login</button>
+                <button type="submit" className="btn btn-outline-primary">Login</button>
               </form>
           </div>
         </div>
