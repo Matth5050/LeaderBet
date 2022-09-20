3a287db3b582382ae7dd75bc74883b1bb9fc63b8
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-19 21:46:19 -0700

    added MainPageControl to better contain main components

--- leader-bet/src/components/MainPageControl.js
+++ leader-bet/src/components/MainPageControl.js
@@ -1,15 +1,40 @@
-import React from "react";
-
-class MainPageControl extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedTicket: null,
-      editing: false
-    };
-  };
+import React, { useContext } from "react";
+import Odds from "./Odds"
+import LeaderBoard from "./LeaderBoard";
+import { Link } from "react-router-dom";
+import { auth } from './../firebase.js'
+import { UserContext } from './UserContext';
 
 
-  
+function MainPageControl() {
+
+  const {isLogged, setIsLogged, userName, setUserName} = useContext(UserContext);
+
+  if (isLogged === false) {
+    return (
+      <React.Fragment>
+        <h1>You must be signed in to access the queue.</h1>
+      </React.Fragment>
+    )
+  } else if (isLogged === true) {
+  return (
+    <React.Fragment>
+      <div className="bg">
+    <div className='container'>
+      <div className='row mt-5'>
+        <div className='col-9'>
+          <Odds />
+        </div>
+        <div className='col-3'>
+          <LeaderBoard />
+        </div>
+      </div>
+    </div> 
+
+      </div>
+  </React.Fragment>
+  )
+  }
 }
+
+export default MainPageControl;
