3958312df12f3d2403eb562fcb601f8db86e68f3
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-19 21:44:33 -0700

    added new imports and context hooks

--- leader-bet/src/components/App.js
+++ leader-bet/src/components/App.js
@@ -1,31 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MainPageControl from './MainPageControl';
 import Header from './Header';
-import LeaderBoard from './LeaderBoard';
-import Odds from './Odds';
 import Footer from './Footer';
 import Login from './Login';
 import Register from './Register';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {UserContext} from "./UserContext";
+import "bootstrap/dist/js/bootstrap.min.js";
+
 
 function App() {
+
+const [isLogged, setIsLogged] = useState(false);
+const [userName, setUserName] = useState(null);
+
   return (
     <Router>
-     
-        <Routes>
-            {/* <div className='container'>
-                  <div className='row mt-5'>
-                    <div className='col-9'>
-                      <Odds />
-                    </div>
-                    <div className='col-3'>
-                      <LeaderBoard />
-                    </div>
-                  </div>
-                </div> */}
-          <Route path="/" element={<Odds />} />    
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <UserContext.Provider value={{isLogged, setIsLogged, userName, setUserName}}>
+          <Header />
+          <Routes>
+            <Route path="/" element={<MainPageControl />} />    
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </UserContext.Provider>
         <Footer />
     </Router>
   );
