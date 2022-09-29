f50f47431bfee92748ce8ef8a807779547f0c996
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-28 21:34:47 -0700

    added database query and list format

--- leader-bet/src/components/LeaderBoard.js
+++ leader-bet/src/components/LeaderBoard.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import  { db, auth } from './../firebase.js';
+import { collection, addDoc, onSnapshot, doc, updateDoc, deleteDoc } from 'firebase/firestore';
 
 function LeaderBoard() {
+
+  const [mainAccountsList, setMainAccountsList ] = useState([]);
+
+  useEffect(() => {
+    const unSubscribe = onSnapshot(
+      collection(db, "accounts"),
+      (collectionSnapshot) => {
+        const accounts = [];
+        collectionSnapshot.forEach((doc) => {
+          accounts.push({
+            email: doc.data().email,
+            userName: doc.data().userName,
+            date: doc.data().date,
+            win: doc.data().win,
+            loss: doc.data().loss,
+            id: doc.id
+          });
+        });
+        setMainAccountsList(accounts);
+      },
+      (error) => {
+
+      }
+    );
+    return () => unSubscribe();
+  }, []);
+
+
+//
+
   return (
     <React.Fragment>
       <div className="card">
         <div className="card-header">
           Leader Board
         </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">Player 1</li>
-          <li className="list-group-item">Player 2</li>
-          <li className="list-group-item">Player 3</li>
-        </ul>
+        <ol start="1" className="firstItem">
+          {mainAccountsList.map(({userName}, i) => { 
+            if (i === 0) {
+              return (
+                <li className='secondItem px-0'>{userName}</li>)
+            } else
+            return (
+            <li className=''>{userName}</li>
+          )})}
+        </ol>
       </div>
 
     </React.Fragment>
