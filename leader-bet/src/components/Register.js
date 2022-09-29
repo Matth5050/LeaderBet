4597721aae265f40bb97f9acee6b4887db0c628a
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-28 21:35:23 -0700

    added function to added new document to databse on account cretion

--- leader-bet/src/components/Register.js
+++ leader-bet/src/components/Register.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
+import  { db } from './../firebase.js';
+import  {collection, addDoc } from 'firebase/firestore';
+import { v4 } from 'uuid';
 import { auth } from "./../firebase.js";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import Header from "./Header.js";
 
 function Register() {
+
+
+  // const navigate =useNavigate();
+  // const toHome = useCallback(() => navigate('/login', {replace:true}, [navigate]));
 
   const cardWidth = {
     width: "18rem",
@@ -12,16 +20,28 @@ function Register() {
 
   const [signUpSuccess, setSignUpSuccess] = useState(null);
 
+  function onNewAccountCreation(email, userName){
+    addDoc(collection(db,'accounts'),{
+      email:email,
+      userName: userName,
+      win: 0,
+      loss: 0,
+      id: v4()
+    })}
+
   function doSignUp(event) {
     event.preventDefault();
     console.log('click');
     const email = event.target.email.value;
+    const userName = event.target.userName.value;
     const password = event.target.password.value;
     const password2 = event.target.password2.value;
     if (password === password2) {
         createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        setSignUpSuccess(`You've successfully signed up, ${userCredential.user.email}!`)
+        setSignUpSuccess(`You've successfully signed up, ${userCredential.user.email}!`);
+        onNewAccountCreation(`${userCredential.user.email}`, `${userName}`);
+        
       })
       .catch((error) => {
         setSignUpSuccess(`There was an error signing up: ${error.message}!`)
@@ -33,12 +53,22 @@ function Register() {
 
   return (
     <React.Fragment>
+      <Header />
       <div>
       {signUpSuccess}
         <div className="card text-center mx-auto p-3" style={cardWidth}>
           <div className="">
             <h3 className="mb-3 card-title">Register</h3>
             <form className="form" onSubmit={doSignUp}>
+                <div className="mb-3">
+                  <input 
+                    type="text" 
+                    className="form-control col-xs-3" 
+                    id="exampleInputUserName" size="50"
+                    name="userName" 
+                    aria-describedby="userNameHelp" 
+                    placeholder="User Name"></input>
+                </div>
                 <div className="mb-3">
                   <input 
                     type="text" 
@@ -73,5 +103,6 @@ function Register() {
     </React.Fragment>
   );
 };
+
 
 export default Register;
