98d9597a752a825f35128e385a01afaf935fcdf0
Matth5050 <matthew.f.herbert@gmail.com>
2022-09-30 10:37:09 -0700

    added new component

---
+++ leader-bet/src/components/BetSlip.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function BetSlip() {
+  
+  return (
+    <React.Fragment>
+      <div class="card mt-5">
+        <h5 class="card-header">Open Bets</h5>
+        <div className="card-body">
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default BetSlip;
