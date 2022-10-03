3c4cd2e22f00370fa8d36e3765e21f65243ae39e
Matth5050 <matthew.f.herbert@gmail.com>
2022-10-03 13:45:29 -0700

    changed styling

--- leader-bet/src/components/BetSlip.js
+++ leader-bet/src/components/BetSlip.js
@@ -4,7 +4,7 @@ function BetSlip() {
   
   return (
     <React.Fragment>
-      <div class="card mt-5">
+      <div class="card mt-5 text-bg-dark">
         <h5 class="card-header">Open Bets</h5>
         <div className="card-body">
           <p class="card-text">Some bets will be mapped here</p>
